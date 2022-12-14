6f2cb1cfb7053418fe9a91cf1e230cb60d3d930f
luminecs <wangxuke.china@gmail.com>
2022-12-14 08:38:13 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Index from './components'
-import MyPaper from './components/MyPaper'
+import MyPaper from './components/mui/MyPaper'
+import Sx from './components/mui-system/sx'
 
 function App () {
-
   return (
     <Routes>
       <Route path="/" element={<Index/>}/>
       <Route path="/mui/paper" element={<MyPaper/>}/>
+      <Route path="/mui/system/sx" element={<Sx/>}/>
     </Routes>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,11 @@ import { Link as RRLink } from 'react-router-dom'
 export default function Index () {
   return (
     <>
+      <h2>MUI</h2>
       <RRLink to="/mui/paper">Paper</RRLink>
+
+      <h2>MUI-system</h2>
+      <RRLink to="/mui/system/sx">sx prop</RRLink>
     </>
   )
 }
